174795a276404fa71c2892c8071245da034bba65
Adrian Samaniego <llathander@gmail.com>
2020-01-17 22:15:31 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -21,8 +21,29 @@ def using_insert(array, element)
   array.insert(4, element)
 end
 
+def using_uniq(array)
+  array.uniq
+end
 
-    
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete_at(integer)
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
+end
 
 #def using_push(array, string)
 #  array.push(string)
